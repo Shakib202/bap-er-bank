66ef2a129b70376c4a6b5408609f1a46adeacc52
Shakib202 <shakib2020360@gmail.com>
2022-03-09 05:27:43 +0600

    first commit

--- js/banking.js
+++ js/banking.js
@@ -29,20 +29,16 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
   const withdrawAmountText = withdrawInput.value;
   const newWithdrawAmount = parseFloat(withdrawAmountText);
   console.log(newWithdrawAmount);
-
   // set withdraw total
   const withdrawTotal = document.getElementById('withdraw-total');
   const previousWithdrawText = withdrawTotal.innerText;
   const previousWithdrawTotal = parseFloat(previousWithdrawText);
   const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
   withdrawTotal.innerText = newWithdrawTotal;
-
   // update balance
   const balanceTotal = document.getElementById('balance-total');
   const previousBalanceText = balanceTotal.innerText;
   const previousBalanceTotal = parseFloat(previousBalanceText);
   const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-
   balanceTotal.innerText = newBalanceTotal;
-  
 })
